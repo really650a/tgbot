27eba26f741c5ec92445282349bceafa45bbc233
really650a <99811385+really650a@users.noreply.github.com>
2023-08-06 10:05:29 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,26 +1,26 @@
 from flask import Flask, request
-import telebot
+from telebot import Telebot 
 from telebot.types import *
 
 TOKEN = "6457745689:AAGK_N4F-8KPw7zpnGf8NfFZrpTD2RhkotM"
-bot = telebot.TeleBot(TOKEN, parse_mode="html")
+bot = TeleBot(TOKEN, parse_mode="html")
 server = Flask(__name__)
 
 @bot.message_handler(commands =["start"])
 def start(message):
 	bot.send_message(message.chat.id, "Hello!")
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    if update.message:
-        bot.process_new_messages([update.message])
+#@server.route('/' + TOKEN, methods=['POST'])
+#def getMessage():
+    #update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    #if update.message:
+        #bot.process_new_messages([update.message])
         
-    return "ok", 200
+    #return "ok", 200
 
 @server.route("/")
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://tgbot-flame.vercel.app/' + TOKEN)
+    #bot.remove_webhook()
+    #bot.set_webhook(url='https://tgbot-flame.vercel.app/' + TOKEN)
     return "ok", 200
 
