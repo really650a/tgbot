ac6b918e38bd8d159063d0c160d1062a196bead4
really650a <99811385+really650a@users.noreply.github.com>
2023-08-06 10:15:10 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+#from flask import Flask, request
 #from telebot import Telebot 
 #from telebot.types import *
 
 #TOKEN = "6457745689:AAGK_N4F-8KPw7zpnGf8NfFZrpTD2RhkotM"
 #bot = TeleBot(TOKEN, parse_mode="html")
-server = Flask(__name__)
+#server = Flask(__name__)
 
 #@bot.message_handler(commands =["start"])
 #def start(message):
@@ -18,9 +18,18 @@ server = Flask(__name__)
         
     #return "ok", 200
 
-@server.route("/")
-def webhook():
+#@server.route("/")
+#def webhook():
     #bot.remove_webhook()
     #bot.set_webhook(url='https://tgbot-flame.vercel.app/' + TOKEN)
-    return "ok", 200
+    #return "ok", 200
 
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
