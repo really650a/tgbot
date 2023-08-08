a357bde37affdad8cdf5623fe077097887586317
really650a <99811385+really650a@users.noreply.github.com>
2023-08-08 07:56:04 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -26,5 +26,3 @@ def set_webhook():
     bot.set_webhook(url='https://tgbot-46vemn21f-really650a.vercel.app/' + TOKEN)
     return "Webhook set up successfully!", 200
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
