b31436e793fdba10f07efe26a415ec9da01fef53
really650a <99811385+really650a@users.noreply.github.com>
2023-08-08 07:15:18 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+"""from flask import Flask, request
 from telebot import TeleBot 
 from telebot.types import *
 
@@ -23,4 +23,39 @@ def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://tgbot-gamma-seven.vercel.app/' + TOKEN)
     return "Hello World!", 200
+"""
+from flask import Flask, request
+from telebot import TeleBot 
+from telebot.types import *
+
+TOKEN = "5769907387:AAF0tVVa2RNQjFpOeYmRAIWBhzIBa1jFp4E"
+bot = TeleBot(TOKEN, parse_mode="html")
+app = Flask(__name__)
+
+last_update_id = None
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    return bot.send_message(message.chat.id, "Hello!")
+
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    global last_update_id
+
+    update = Update.de_json(request.stream.read().decode('utf-8'))
+
+    if update.message and (last_update_id is None or update.update_id > last_update_id):
+        last_update_id = update.update_id
+        bot.process_new_messages([update.message])
+
+    return "Hello World!", 200
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your-webhook-url.com/' + TOKEN)
+    return "Hello World!", 200
+
+if __name__ == "__main__":
+    app.run()
 
